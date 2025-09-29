f2e1d9457f6f56c9942a2a454f41872ccf244740
ZBG11 <zombiegamez11@gmail.com>
2025-09-29 23:06:55 +0700

    Models

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Car
+{
+	public int CarID { get; set; }
+	public int Seat { get; set; }
+	public string TypeCar { get; set; }
+	public double Price { get; set; }
+	public string NameCar { get; set; }
+	public DateTime ModelYear { get; set; }
+	public bool State { get; set; }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Users
+{
+	public int ID { get; set; }
+	public int RoleID { get; set; }
+	public int PhoneNumber { get; set; }
+	public string LastName { get; set; }
+	public string First Name { get; set; }
+	public string Email { get; set; }
+	public string Sex { get; set; }
+	public DateTime Birthday { get; set; }
+	public DateTime JoinDate { get; set; }
+}
