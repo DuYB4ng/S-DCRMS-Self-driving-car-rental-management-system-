bcd043fc6c9a1823bc6bd681ee05dc39f8ee0ccd
ZBG11 <zombiegamez11@gmail.com>
2025-09-29 23:06:55 +0700

    Models

---
+++ Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public int BookingID { get; set; }
+	public int UserID { get; set; }
+	public int CarID { get; set; }
+	public DateTime StartDate { get; set; }
+	public DateTime EndDate { get; set; }
+	public double TotalPrice { get; set; }
+	public string Status { get; set; }
+	public string Description { get; set; }
+	public string Author { get; set; }
+	public string Title { get; set; }
+	public string Url { get; set; }
+	public string Url2 { get; set; }
+
+}
