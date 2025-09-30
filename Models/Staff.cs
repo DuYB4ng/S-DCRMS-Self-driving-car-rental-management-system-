080a31f4cfba25408b5396a1f7de687987060c15
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-09-30 21:02:21 +0700

    check branch trieu

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -3,10 +3,10 @@
 public class Staff
 {
     public int StaffID { get; set; }
-    public string LastName { get; set; }
-    public string FirstName { get; set; }
-    public string Position { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string Position { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
     public DateTime HireDate { get; set; }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -2,13 +2,13 @@
 
 public class Users
 {
-	public int ID { get; set; }
-	public int RoleID { get; set; }
-	public int PhoneNumber { get; set; }
-	public string LastName { get; set; }
-	public string First Name { get; set; }
-	public string Email { get; set; }
-	public string Sex { get; set; }
-	public DateTime Birthday { get; set; }
-	public DateTime JoinDate { get; set; }
+    public int ID { get; set; }
+    public int RoleID { get; set; }
+    public int PhoneNumber { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Sex { get; set; } = string.Empty;
+    public DateTime Birthday { get; set; }
+    public DateTime JoinDate { get; set; }
 }
