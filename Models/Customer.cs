0c4353e543b2574bcd5c52f8ed9f90a92fc37e76
toiloi <anhtoai847@gmail.com>
2025-09-30 22:22:27 +0700

    hoan thien models

---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace SDCRMS.Models
+{
+    public class Customer : Users
+    {
+        public string DrivingLisence { get; set; }
+        public DateTime LisenceIssueDate { get; set; }
+        public DateTime LisenceExpiryDate { get; set; }
+    }
+}
