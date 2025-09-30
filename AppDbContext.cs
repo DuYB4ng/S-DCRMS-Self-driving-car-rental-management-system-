2a1c66c42ce1d45064ad5fe0e9ddfccb7a2120ec
toiloi <anhtoai847@gmail.com>
2025-09-30 22:59:21 +0700

    lien ket database

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDCRMS.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Users> Users { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<OwnerCar> OwnerCars { get; set; }
+    public DbSet<Payment> Paytments { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+}
