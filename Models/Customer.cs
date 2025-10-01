7452c455c74babe493923c3d1a61d2ebd2e903ef
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-10-01 17:18:28 +0700

    them CustomID cho Models

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,11 @@
-﻿namespace SDCRMS.Models
+﻿using System.Threading.Tasks;
+
+namespace SDCRMS.Models
 {
     public class Customer : Users
     {
-        public string DrivingLisence { get; set; }
+        public int CustomerID { get; set; }
+        public string DrivingLisence { get; set; } = string.Empty;
         public DateTime LisenceIssueDate { get; set; }
         public DateTime LisenceExpiryDate { get; set; }
     }
