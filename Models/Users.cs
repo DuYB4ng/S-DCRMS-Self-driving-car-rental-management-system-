3fcd875889426290c32a09bd921ed781cab4d99e
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-10-01 19:33:50 +0700

    up thêm controllers admin và notification

--- Models/Users.cs
+++ Models/Users.cs
@@ -4,11 +4,11 @@ public class Users
 {
     public int ID { get; set; } = 0;
     public int RoleID { get; set; } = 0;
-    public int PhoneNumber { get; set; } = 0;
-    public string LastName { get; set; } = string.Empty;
-    public string FirstName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Sex { get; set; } = string.Empty;
-    public DateTime Birthday { get; set; } = DateTime.UtcNow;
-    public DateTime JoinDate { get; set; } = DateTime.Now;
+    public required int PhoneNumber { get; set; }
+    public required string LastName { get; set; }
+    public required string FirstName { get; set; }
+    public required string Email { get; set; }
+    public required string Sex { get; set; }
+    public DateTime Birthday { get; set; }
+    public DateTime JoinDate { get; set; }
 }
