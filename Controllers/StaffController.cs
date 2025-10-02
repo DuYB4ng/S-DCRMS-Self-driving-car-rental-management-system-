b5030a19ce987d190a37bcb5dae0f7cb66a88c4e
DuYB4ng <quynhphung11@gmail.com>
2025-10-02 22:33:35 +0700

    Tao API lay danh sach customer

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+namespace SDCRMS.Controllers
+{
+    [Route("api/staff")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public StaffController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var customers = _context.Customers.ToList();
+
+            return Ok(customers);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetID([FromRoute] int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+    }
+}
