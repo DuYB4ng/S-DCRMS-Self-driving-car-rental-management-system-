39101eae8f563b5163b0584f8a4e48126e9209e7
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-10-05 21:50:57 +0700

    Cap nhat lai notification

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,9 +1,6 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SDCRMS.DTOs.Notification;
 using SDCRMS.Mappers;
-using SDCRMS.Models;
 
 namespace SDCRMS.Controllers
 {
