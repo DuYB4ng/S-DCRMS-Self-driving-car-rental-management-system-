c6836a31f8d143b8e06839083f34a3d3a2df4eab
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-10-05 07:00:33 +0700

    Cap nhat Controller và CreateDto

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SDCRMS.DTOs.Notification;
+using SDCRMS.Mappers;
 using SDCRMS.Models;
 
 namespace SDCRMS.Controllers
@@ -10,80 +11,32 @@ namespace SDCRMS.Controllers
     [Route("api/[controller]")]
     public class NotificationController : ControllerBase
     {
-        private static List<Notification> notifications = new List<Notification>();
+        private readonly AppDbContext _context;
+
+        public NotificationController(AppDbContext context)
+        {
+            _context = context;
+        }
 
         // Get: api/notification
         [HttpGet]
-        public ActionResult<IEnumerable<Notification>> GetNotifications()
+        public async Task<IActionResult> GetNotifications()
         {
-            if (notifications.Count == 0)
-            {
-                return NotFound("Không tìm thấy thông báo nào.");
-            }
-
-            return notifications;
+            var notifications = await _context.Notifications.ToListAsync();
+            var dtos = notifications.Select(n => n.ToNotificationDto()).ToList();
+            return Ok(dtos);
         }
 
         // Get: api/notification/{id}
         [HttpGet("{id}")]
-        public ActionResult<Notification> GetNotificationById(int id)
+        public async Task<IActionResult> GetNotification(int id)
         {
-            var notification = notifications.FirstOrDefault(n => n.NotificationID == id);
+            var notification = await _context.Notifications.FindAsync(id);
             if (notification == null)
             {
-                return NotFound($"Không tìm thấy thông báo có ID {id}");
+                return NotFound();
             }
-
-            return notification;
-        }
-
-        // Post: api/notification
-        [HttpPost]
-        public ActionResult<Notification> PostNotification(Notification notification)
-        {
-            var newNotification = new Notification
-            {
-                NotificationID =
-                    notifications.Count > 0 ? notifications.Max(n => n.NotificationID) + 1 : 1,
-                UserID = notification.UserID,
-                Title = notification.Title,
-                Message = notification.Message,
-                LinkURL = notification.LinkURL,
-            };
-            notifications.Add(newNotification);
-            return Ok(newNotification);
-        }
-
-        // PUT: api/notification/{id}
-        [HttpPut("{id}")]
-        public ActionResult<Notification> PutNotification(int id, Notification notification)
-        {
-            var notificationEdit = notifications.SingleOrDefault(n => n.NotificationID == id);
-            if (notificationEdit == null)
-            {
-                return NotFound($"Không tìm thấy thông báo có ID {id}");
-            }
-
-            // Cập nhật các trường của thông báo
-            notificationEdit.UserID = notification.UserID;
-            notificationEdit.Title = notification.Title;
-            notificationEdit.Message = notification.Message;
-            notificationEdit.LinkURL = notification.LinkURL;
-
-            return Ok(notificationEdit);
-        }
-
-        // DELETE: API/notification/{id}
-        [HttpDelete("{id}")]
-        public ActionResult DeleteNotification(int id)
-        {
-            var notificaltionRemove = notifications.FirstOrDefault(n => n.NotificationID == id);
-            if (notificaltionRemove == null)
-            {
-                return NotFound($"Không tìm thấy thông báo có id: {id}");
-            }
-            notifications.Remove(notificaltionRemove);
-            return Ok($"Xóa thành công thông báo có id: {id}");
+            return Ok(notification.ToNotificationDto());
         }
     }
 }
