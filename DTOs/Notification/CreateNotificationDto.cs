488820348f510c19648f7881174518e96c6f4bd7
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-10-05 07:00:33 +0700

    Cap nhat Controller và CreateDto

--- DTOs/Notification/CreateNotificationDto.cs
+++ DTOs/Notification/CreateNotificationDto.cs
@@ -5,8 +5,8 @@ namespace SDCRMS.DTOs.Notification
     public class CreateNotificationDto
     {
         public int UserID { get; set; }
-        public string Message { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
         public string LinkURL { get; set; } = string.Empty;
     }
 }
