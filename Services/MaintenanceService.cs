24f6a8039e642dea3ae7e94c6eb13defdd4a718f
ViAn-Monsieur <tranvian.dev@gmail.com>
2025-10-09 11:19:26 +0700

    fix-bug

---
+++ Services/MaintenanceService.cs
@@ -0,0 +1,14 @@
+using SDCRMS.Dtos.Car;
+using SDCRMS.Models;
+using SDCRMS.Repositories;
+using AutoMapper;
+namespace SDCRMS.Services
+{
+    public interface IMaintenanceService
+    {
+    }
+    public class MaintenanceService : IMaintenanceService
+    {
+
+    }
+}
