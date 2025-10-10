1a984e4018a72898694b1f4dcd42d9ab4da41c9d
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-10-10 22:58:25 +0700

    Sua admin va them mk hash

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,13 +1,16 @@
-﻿namespace SDCRMS.Models
+﻿using SDCRMS.Models.Enums;
+
+namespace SDCRMS.Models
 {
     public class Users
     {
         public int UserID { get; set; }
-        public int RoleID { get; set; }
+        public UserRole Role { get; set; }
         public int PhoneNumber { get; set; }
         public string LastName { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty; // Thêm mật khẩu hash
         public string Sex { get; set; } = string.Empty;
         public DateTime Birthday { get; set; }
         public DateTime JoinDate { get; set; }
