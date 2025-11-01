a4a797a7928fb0a3d35273c50f36f74cdce001ff
ViAn-Monsieur <tranvian.dev@gmail.com>
2025-11-02 00:13:45 +0700

    cau hinh api gateway + firebase jwt

--- Client/sdcrms-web/src/routes.jsx
+++ Client/sdcrms-web/src/routes.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import OwnerCarPage from "./pages/OwnerCarPage";
+import Login from "./pages/Login";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,// Layout chung
     children: [
       { path: "/", element: <App/> },
-      { path: "/owner", element: <OwnerCarPage/>}
+      { path: "/owner", element: <OwnerCarPage/> },
+      { path: "/login", element: <Login/> }
     ],
   },
 ]);
