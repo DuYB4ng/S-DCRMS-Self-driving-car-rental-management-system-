e698cb234a7cfab42220aa33c8f4c9ac0fc744e8
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-11-04 15:59:44 +0700

    Doi tu int sang string o PhoneNumber

--- DTOs/Admin/AdminDto.cs
+++ DTOs/Admin/AdminDto.cs
@@ -7,7 +7,7 @@ namespace SDCRMS.DTOs.Admin
         public string LastName { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
         public string Sex { get; set; } = string.Empty;
         public DateTime Birthday { get; set; }
         public string Address { get; set; } = string.Empty;
