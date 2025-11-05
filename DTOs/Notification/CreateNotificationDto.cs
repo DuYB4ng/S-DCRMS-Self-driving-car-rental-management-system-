2d683c29ade44c31536ee8ce9dd658801ae55253
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-11-05 16:30:42 +0700

    delete

--- DTOs/Notification/CreateNotificationDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SDCRMS.DTOs.Notification
-{
-    public class CreateNotificationDto
-    {
-        public int NotificationID { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Message { get; set; } = string.Empty;
-    }
-}
