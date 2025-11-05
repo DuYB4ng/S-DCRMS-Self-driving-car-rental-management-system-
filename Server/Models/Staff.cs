56698d2bbe0ab01e02d96cec3d0d47b9057338e3
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-11-05 20:22:42 +0700

    Update file Model

---
+++ Server/Models/Staff.cs
@@ -0,0 +1,13 @@
+﻿namespace SDCRMS.Models
+{
+    public class Staff : Users
+    {
+        private void quanLyTaiKhoanKhachHang() { }
+
+        private void theoDoiHoatDongDatXePhanHoi() { }
+
+        private void guiThongBao() { }
+
+        private void baoCaoThongKe() { }
+    }
+}
