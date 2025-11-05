1d1fe9370500cd4a7693832b59d6e0de8f1dfe98
ViAn-Monsieur <tranvian.dev@gmail.com>
2025-11-05 17:08:29 +0700

    cap nhat auth service

--- Server/AuthService/Program.cs
+++ Server/AuthService/Program.cs
@@ -1,12 +1,20 @@
 using AuthService.Repositories;
 using Microsoft.EntityFrameworkCore;
-
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using AuthService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddOpenApi();
+// Chỉ khởi tạo Firebase nếu chưa khởi tạo và file tồn tại
+if (File.Exists("firebase-adminsdk.json") && FirebaseApp.DefaultInstance == null)
+{
+    FirebaseApp.Create(new AppOptions
+    {
+        Credential = GoogleCredential.FromFile("firebase-adminsdk.json")
+    });
+}
 
-// Đăng ký DbContext với connection string
+// Đăng ký DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -15,36 +23,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Đăng ký Repository
 builder.Services.AddScoped<IAuthUserRepository, AuthUserRepository>();
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        )).ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
