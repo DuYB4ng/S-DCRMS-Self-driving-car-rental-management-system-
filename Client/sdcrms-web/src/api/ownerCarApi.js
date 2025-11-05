80887445900ac9f6e94f3d23a2de6ed931c4a5a4
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-11-05 14:23:07 +0700

    checkout tu branch 'an'

---
+++ Client/sdcrms-web/src/api/ownerCarApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_BASE = `${import.meta.env.VITE_API_BASE_URL}/OwnerCar`;// Đổi theo port thật
+
+export const getAllOwnerCars = async () => {
+  const res = await axios.get(`${API_BASE}`);
+  return res.data;
+};
+
+export const createOwnerCar = async (data) => {
+  const res = await axios.post(`${API_BASE}`, data);
+  return res.data;
+};
+
+export const updateOwnerCar = async (id, data) => {
+  const res = await axios.put(`${API_BASE}/${id}`, data);
+  return res.data;
+};
+
+export const deleteOwnerCar = async (id) => {
+  const res = await axios.delete(`${API_BASE}/${id}`);
+  return res.data;
+};
