1161279b672fde44f270cd750756af8822292ebc
ViAn-Monsieur <tranvian.dev@gmail.com>
2025-11-05 13:03:14 +0700

    login page

--- Client/sdcrms-web/src/routes.jsx
+++ Client/sdcrms-web/src/routes.jsx
@@ -1,15 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import OwnerCarPage from "./pages/OwnerCarPage";
 import Login from "./pages/Login";
+
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,// Layout chung
-    children: [
-      { path: "/", element: <App/> },
-      { path: "/owner", element: <OwnerCarPage/> },
-      { path: "/login", element: <Login/> }
-    ],
-  },
+  { path: "/owner", element: <OwnerCarPage /> },
+  { path: "/login", element: <Login /> },
 ]);
