901d4b8110d134602443e84ba551538f58d3704b
toiloi <anhtoai847@gmail.com>
2025-11-13 21:19:45 +0700

    them customer con bug

---
+++ Server/CustomerService/Dtos/Customer/UpdateCustomerDto.cs
@@ -0,0 +1,13 @@
+﻿using CustomerService.Models;
+
+namespace CustomerService.Dtos.Customer
+{
+    public class UpdateCustomerDto
+    {
+        public int CustomerId { get; set; }
+        public string FirebaseUid { get; set; } = string.Empty; 
+        public string DrivingLicense { get; set; } = string.Empty;
+        public DateTime LicenseIssueDate { get; set; }
+        public DateTime LicenseExpiryDate { get; set; }
+    }
+}
