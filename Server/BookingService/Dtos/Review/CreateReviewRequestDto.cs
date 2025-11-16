c4793fc8786da9f4bddf647c5a3662e101c6d56f
toiloi <anhtoai847@gmail.com>
2025-11-16 21:04:22 +0700

    hoan thien call api

--- Server/BookingService/Dtos/Review/CreateReviewRequestDto.cs
+++ Server/BookingService/Dtos/Review/CreateReviewRequestDto.cs
@@ -2,10 +2,10 @@ namespace BookingService.Dtos.Review
 {
     public class CreateReviewRequestDto
     {
-        public int ReviewID { get; set; }
         public int Rating { get; set; }
         public string Comment { get; set; }
         public DateTime ReviewDate { get; set; } = DateTime.Now;
+        public int BookingID { get; set; }
     }
     
 }
--- Server/BookingService/Dtos/Review/UpdateReviewRequestDto.cs
+++ Server/BookingService/Dtos/Review/UpdateReviewRequestDto.cs
@@ -2,9 +2,9 @@ namespace BookingService.Dtos.Review
 {
     public class UpdateReviewRequestDto
     {
-        public int ReviewID { get; set; }
         public int Rating { get; set; }
         public string Comment { get; set; }
+        public int BookingID { get; set; }
 
     }
 }
