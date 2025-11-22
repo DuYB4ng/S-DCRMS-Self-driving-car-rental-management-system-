35fc9c19ce25b7fd00f746320a2074328cd1fced
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-11-22 18:29:41 +0700

    hoang thanh call api cho notification

--- Server/NotificationService/Program.cs
+++ Server/NotificationService/Program.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Threading;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
-using Google.Apis.Auth.OAuth2;
 using Google.Apis.Auth.OAuth2.Flows;
 using Google.Apis.Auth.OAuth2.Responses;
 // using Microsoft.AspNetCore.Authentication.JwtBearer;
